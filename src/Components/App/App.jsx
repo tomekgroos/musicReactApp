071a01602905e5a1feac5e2b18ac29da0e46a783
Your Name <tomek.groos@gmail.com>
2021-01-16 21:07:37 +0100

    adding components

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import './App.css';
 import Logo from '../img/logo white.png';
 
+import PlayList from "../PlayList/PlayList";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: []
+    }
+  }
 
 render() {
   return (
     
     <div>
   <img className="logo" src={Logo} alt="logo"/>
-     <h1>(Not) Spotify Player</h1>
+     <h1 className="highlight">(Not) Spotify Player</h1>
 
-  
-  <h1>Ja<span class="highlight">mmm</span>ing</h1>
   <div class="App">
     {/* Add a SearchBar component */}
-    <div class="App-playlist">
+    <div className="App-playlist">
       {/*  Add a SearchResults component */}
       {/*  Add a Playlist component */}
     </div>
---
+++ src/Components/PlayList/PlayList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./PlayList.css";
+import TrackList from "../TrackList/TrackList";
+
+class PlayList extends React.Component {
+    render() {
+        return (
+            <div className="Playlist">
+  <input defaultValue={"New Playlist"}/>
+  {/* <!-- Add a TrackList component --> */}
+  <button className="Playlist-save">SAVE TO SPOTIFY</button>
+</div>
+        )
+    }
+}
+
+export default PlayList;
