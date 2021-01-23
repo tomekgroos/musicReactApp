bf7d64cafd0f4dc7414f7c7b06c1208a937b8b9b
Your Name <tomek.groos@gmail.com>
2021-01-23 23:51:30 +0100

    spotify module added

---
+++ src/util/Spotify.js
@@ -0,0 +1,25 @@
+let accessToken;
+
+const Spotify  = {
+    getAccessToken(){
+        if (accessToken){
+            return accessToken;
+        }
+
+        // check whether access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch){
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            // clear the parameters
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
+    }
+
+    
+
+export default Spotify;
